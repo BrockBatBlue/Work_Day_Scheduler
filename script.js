3dea90a95f1635b310cfe22568592c0fe47e153e
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-05-07 17:35:48 -0500

    Added two for loops to create the time data, and the rows with buttons. Also, deleted hard code to make sure it works

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Variables
 var currentDay = moment().format('MMMM Do YYYY');
+var tasks = [];
 
 // Create Elements
 $("#currentDay").text(currentDay);
-console.log(currentDay);
+
 
 // Timeblocks
+for(i=9;i<=17; i++) {
+    console.log(moment.utc(i*1000*3600).format('HH:mm'));
+    tasks.push([moment.utc(i*1000*3600).format('HH:mm'),'']);
+}
+console.table(tasks);
+for(i=0;i<tasks.length;i++) {
+    var row = $("<div>").addClass("row");
+    row.append(
+        $("<div>").addClass("col-1 hour").append(
+            $("<p>").text(tasks[i][0])));
+    row.append($("<input>").addClass("col-10 future time-block textarea description").attr("value", tasks[i][1]));
+    row.append($("<div>").addClass("col-1 saveBtn fas fa-save"));  
+    $(".container").append(row);
+}
+
 
 // Local Storage
 
