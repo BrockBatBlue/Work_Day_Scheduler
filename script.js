0d820fb345b0af2c9c3b1c38b9476697ce3262a5
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-05-07 16:57:38 -0500

    Changed CSS and made temporary hard code for visual purposes, and added the current day using JQ text

---
+++ script.js
@@ -0,0 +1,12 @@
+// Variables
+var currentDay = moment().format('MMMM Do YYYY');
+
+// Create Elements
+$("#currentDay").text(currentDay);
+console.log(currentDay);
+
+// Timeblocks
+
+// Local Storage
+
+// Event Listener Section
